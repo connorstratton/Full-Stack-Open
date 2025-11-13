47ae5d72b36454f7dd2396291ab72a2f8721e2c0
connorstratton <148221280+connorstratton@users.noreply.github.com>
2025-11-12 22:45:33 -0500

    Add files via upload

---
+++ part1/anecdotes/App.jsx
@@ -0,0 +1,89 @@
+import { useState } from 'react'
+
+const Button = (props) => {
+  return(
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const getRandomInteger = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const Stats = (props) => {
+  return(
+    <div>
+      <p>has {props.num} votes</p>
+    </div>
+  )
+}
+
+const Largest = (props) => {
+  const maxVal = Math.max(...props.votes)
+  const i = props.votes.indexOf(maxVal)
+
+  if (maxVal === 0)
+  {
+    return (
+      <p>no votes yet</p>
+    )
+  }
+
+  return(
+    <div>
+      <p>{props.anecs[i]}</p>
+      <Stats num={props.votes[i]}/>
+    </div>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [randInt, setRandInt] = useState(getRandomInteger(0, anecdotes.length - 1))
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+  
+  console.log(votes)
+
+  const handleNext = (arr) => {
+    const newRand = getRandomInteger(0, arr.length - 1)
+    setRandInt(newRand)
+    setSelected(newRand)
+  } 
+
+  const Vote = (index, arr) => {
+    const newVotes = [...arr ]
+    newVotes[index] += 1
+    setVotes(newVotes)
+  }
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      {anecdotes[selected]}
+      <Stats num={votes[randInt]}/>
+      <br></br>
+      <Button onClick={() => Vote(randInt, votes)} text="vote" />
+      <Button onClick={() => handleNext(anecdotes)} text="next anecdote"/>
+
+      <h1>Anecdote with most votes</h1>
+      <Largest votes={votes} anecs={anecdotes}/>
+    </div>
+  )
+}
+
+export default App
